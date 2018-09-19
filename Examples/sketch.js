e2c983da08a825c13ff646144591b6324abc7c67
Normandanny <NormandannyEOF@gmail.com>
2018-09-18 20:40:39 -0400

    Fixes

--- Examples/sketch.js
+++ Examples/sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 800);
     // v4 Callback way
     d3.csv('', (error, data) => {
       if (error) throw error
@@ -7,9 +7,37 @@ function setup() {
     })
     
     // v5 Promises way
-    // d3.csv('').then(data => {}) 
+    // d3.csv('').then(data => {
+    // console.log(data)
+    // }).catch(e => {consolg.log(e)})
+    // }) 
+
+    // Promise.all(
+    //     [
+    //         d3.csv(''),
+    //         d3.csv(''),
+    //     ]
+    // )
   }
   
   function draw() {
     background(220);
+  }
+
+  function processData(data) {
+      const mapped = data.map(d=>{
+          return {
+              'country': d['country/region'],
+              'population': +d['estimate'].replace(/, /g, ''),
+              'marginOfError': +d.marginOfError
+          }
+      })
+
+      const regionsToFilter = [];
+
+      const noRegions = mapped.filter (d => {
+          return regionsToFilter.every( region => {
+              return d.country.indexOf(region) 
+          })
+      })
   }
