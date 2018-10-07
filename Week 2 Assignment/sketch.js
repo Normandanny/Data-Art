746b28fc8e61dbf6649056e725e11d763a00d4ea
Normandanny <NormandannyEOF@gmail.com>
2018-10-07 18:31:54 -0400

    changes

--- Week 2 Assignment/sketch.js
+++ Week 2 Assignment/sketch.js
@@ -1,52 +1,86 @@
-let populations;
-let objArray = [];
-let value = [];
-let valueRemapped = [];
-let countryName = [];
+var width = 960,
+    height = 500;
 
-function preload() {
-    populations = loadTable("data/simpleData_noRegions.csv", "csv", "header");
-}
+d3.json("Data/App Report.json", function (json) {});
 
-function setup() {
-    console.log(populations.getRowCount() + " total rows in table");
-    for (let i = 0; i < populations.getRowCount(); i++) {
-        let oldObj = populations.getRow(i).obj;
-        let newObj = {};
-        newObj.country = oldObj.country;
-        newObj.estimate = parseInt(oldObj.estimate);
-        newObj.error = parseInt(oldObj.marginOfError);
-        objArray.push(newObj);
-    }
+var nodes = d3.range(10).map(function () {
+        return {
+            radius: Math.random() * 12 + 4
+        };
+    }),
+    root = nodes[0],
+    color = d3.scale.category10();
 
-    for (let i = 0; i < 10; i++) {
-        value.push(objArray[i].estimate)
-    }
+root.radius = 0;
+root.fixed = true;
 
-    for (let i = 0; i < 10; i++) {
-        countryName.push(objArray[i].country)
-    }
+var force = d3.layout.force()
+    .gravity(0.05)
+    .charge(function (d, i) {
+        return i ? 0 : -2000;
+    })
+    .nodes(nodes)
+    .size([width, height]);
 
-    for (let i = 0; i <= 10; i++) {
-        let m = map(value[i], 0, 12000000, 0, 100);
-        valueRemapped.push(m);
-    }
+force.start();
 
-    var ctx = document.getElementById("myChart");
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-var myChart = new Chart(ctx, {
-    data: {
-        datasets: [{
-            data: valueRemapped,
-            backgroundColor: [
-                "#ff6384", "#7CC42C", "#CACC00", "#007AD3", "#3C3C3C", "#EB7013", "#84DB45", "#AC0100", "#FF00FF", "#000000"
-            ]
-        }],
-        labels: countryName
-    },
-        // type: "polarArea",
-        // type: "doughnut",
-        type: "pie"
+svg.selectAll("circle")
+    .data(nodes.slice(1))
+    .enter().append("circle")
+    .attr("r", function (d) {
+        return d.radius;
+    })
+    .style("fill", function (d, i) {
+        return color(i % 3);
+    });
+
+force.on("tick", function (e) {
+    var q = d3.geom.quadtree(nodes),
+        i = 0,
+        n = nodes.length;
+
+    while (++i < n) q.visit(collide(nodes[i]));
+
+    svg.selectAll("circle")
+        .attr("cx", function (d) {
+            return d.x;
+        })
+        .attr("cy", function (d) {
+            return d.y;
+        });
 });
-}
 
+svg.on("mousemove", function () {
+    var p1 = d3.mouse(this);
+    root.px = p1[0];
+    root.py = p1[1];
+    force.resume();
+});
+
+function collide(node) {
+    var r = node.radius + 16,
+        nx1 = node.x - r,
+        nx2 = node.x + r,
+        ny1 = node.y - r,
+        ny2 = node.y + r;
+    return function (quad, x1, y1, x2, y2) {
+        if (quad.point && (quad.point !== node)) {
+            var x = node.x - quad.point.x,
+                y = node.y - quad.point.y,
+                l = Math.sqrt(x * x + y * y),
+                r = node.radius + quad.point.radius;
+            if (l < r) {
+                l = (l - r) / l * .5;
+                node.x -= x *= l;
+                node.y -= y *= l;
+                quad.point.x += x;
+                quad.point.y += y;
+            }
+        }
+        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+    };
+}
